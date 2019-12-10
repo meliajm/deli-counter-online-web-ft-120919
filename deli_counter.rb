b11266f980a8ba5d77aff01a61008d6796fb9e11
melia miller <meliajm@gmail.com>
2019-12-10 05:02:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    string = ""
+    katz_deli.each_with_index do |name, index|
+      
+    puts
+    end
+  end
+end
