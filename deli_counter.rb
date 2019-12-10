453b07d4ab583c8518da317fd87d00e0a9c8fb66
melia miller <meliajm@gmail.com>
2019-12-10 05:16:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli.length == 0 ? puts "There is nobody waiting to be served!" : "other"
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+    
+  end
 end
 
 
